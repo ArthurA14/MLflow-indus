0334c0cee36d26750911303bd395463e0a237a9d
Quentin ANGELOT <57401552+qangelot@users.noreply.github.com>
2022-12-21 19:25:03 +0100

    Update data_prep.py

--- files/data_prep.py
+++ files/data_prep.py
@@ -1,83 +1,95 @@
 import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
 from urllib.parse import urlparse
-import os
+import utils
 import warnings
 warnings.filterwarnings('ignore')
-
 import logging
 
-EXPERIMENT_NAME = "mlflow-demo"
-EXPERIMENT_ID = mlflow.create_experiment(EXPERIMENT_NAME)
 
+# setting the seed
+np.random.seed(40)  
+
+# setting logger 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
-def get_data(URL) :
-    return pd.read_csv(URL, sep=",", header='infer')
+TRAIN_URL = r'../../data/application_train.csv'
+TEST_URL = r'../../data/application_test.csv' 
 
-def write_data(URL, df) : 
-    return df.to_csv(URL, sep=";", index=False, header=True)
 
-# if __name__ == "__main__" :
-warnings.filterwarnings("ignore")
-np.random.seed(40)  
+try:
+    EXPERIMENT_NAME = "mlflow-demo"
+    EXPERIMENT_ID = mlflow.create_experiment(EXPERIMENT_NAME)
+except Exception as e:
+    logger.exception("Experiment ID already set. Error: %s", e)
 
-train_url = r'..\\data\\application_train.csv'
-test_url = r'..\\data\\application_test.csv' 
 
 try :
     # get data
-    train_dataset = get_data(train_url)
-    test_dataset = get_data(test_url)
+    train = utils.get_data(TRAIN_URL)
+    test = utils.get_data(TEST_URL)
 except Exception as e :
     logger.exception("Unable to download training & test CSV. Error: %s", e)
 
+
 # Split the data into training and test sets. (0.80, 0.20) split.
-train, val = train_test_split(train_dataset, test_size=0.20, stratify=train_dataset['TARGET'], random_state=42)
-
-# missing values
-mis_val = train.isnull().sum()
-mis_val_percent = 100.00 * train.isnull().sum() / len(train)
-mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-mis_val_table_rename_columns = mis_val_table.rename(columns={0:'Missing Values', 1:'Percentage'})
-
-mask = mis_val_table_rename_columns.iloc[:,1] != 0
-mis_val_table_rename_columns = mis_val_table_rename_columns[mask].sort_values('Percentage', ascending=False).round(1)
+train, val = train_test_split(train, test_size=0.20, stratify=train['TARGET'], random_state=42)
 
 
-# Encoding categorial variables
-le = LabelEncoder()
-le_cols = 0
+############################ Encodage des variables catégorielles ##########################
+def encoding(train_df, val_df, test_df):
+    """
+    An encoding helper function to create new dataframes with business features
+    
+    Args: 
+        pd.DataFrame: train dataset
+        pd.DataFrame: validation dataset
+        pd.DataFrame: test dataset
+    
+    Returns:
+        pd.DataFrame: three train, validation and test datasets with encoded categorical features
+    """
 
-for col in train:
-    if train[col].dtype == 'object' :
-        if len(list(train[col].unique())) <= 2:
-            # Train on the training data and avoid leakage
-            le.fit(train[col])
-            # Transform both training and testing data
-            train[col] = le.transform(train[col])
-            val[col] = le.transform(val[col])
-            test_dataset[col] = le.transform(test_dataset[col])
-            
-            le_cols += 1
+    print('Training Features shape: ', train_df.shape)
+    print('Training Features shape: ', val_df.shape)
+    print('Testing Features shape: ', test_df.shape)
 
-# one-hot encoding of categorical variables
-train = pd.get_dummies(train)
-val = pd.get_dummies(val)
-test = pd.get_dummies(test_dataset)
+    # label encoding categorial variables if cat count <= 2
+    le = LabelEncoder()
+    le_cols = 0
 
-print('Training Features shape: ', train.shape)
-print('Training Features shape: ', val.shape)
-print('Testing Features shape: ', test.shape)
+    for col in train_df:
+        if train_df[col].dtype == 'object' :
+            if len(list(train_df[col].unique())) <= 2:
+                # Train on the training data and avoid leakage
+                le.fit(train_df[col])
+                # Transform both training and testing data
+                train_df[col] = le.transform(train_df[col])
+                val_df[col] = le.transform(val_df[col])
+                test_df[col] = le.transform(test_df[col])
+                le_cols += 1
 
+    # one-hot encoding of remaining categorical variables
+    train = pd.get_dummies(train_df)
+    val = pd.get_dummies(val_df)
+    test = pd.get_dummies(test_df)
+
+    print('Training Features shape after encoding: ', train.shape)
+    print('Training Features shape after encoding: ', val.shape)
+    print('Testing Features shape after encoding: ', test.shape)
+
+    return train, val, test
+
+train, val, test = encoding(train, val, test)
+
+
+############################ SOME DATAFRAME OPERATIONS ##########################
 
 # remove target 
 train_labels = train['TARGET']
@@ -95,10 +107,15 @@ print('Train df shape: ', train.shape)
 print('Val df shape: ', val.shape)
 print('Test df shape: ', test.shape)
 
-# on indique les valeurs abbérantes avec un booléen 
+
+############################ SOME ANOMALIES OPERATIONS ##########################
+
+# on indique les valeurs abbérantes detectées lors de l'EDA avec un booléen 
 train['DAYS_EMPLOYED_ANOMALIES'] = train["DAYS_EMPLOYED"] == 365243
+
 # on les remplace avec np.nan pour imputation ultérieure
 train['DAYS_EMPLOYED'].replace({365243: np.nan}, inplace = True)
+
 # on réplique les manipulations dans les dfs de val et de test
 val['DAYS_EMPLOYED_ANOMALIES'] = val["DAYS_EMPLOYED"] == 365243
 val["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
