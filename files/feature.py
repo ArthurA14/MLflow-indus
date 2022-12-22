875c381bb87f988712e3f1600085ee22ad10a945
Quentin ANGELOT <57401552+qangelot@users.noreply.github.com>
2022-12-22 13:49:37 +0100

    Update feature.py

--- files/feature.py
+++ files/feature.py
@@ -5,5 +5,9 @@ import utils
 train_enrich = train.copy()
 val_enrich = val.copy()
 test_enrich = test.copy()
+# useful later
+features = list(test_enrich.columns)
 
 train_enrich, val_enrich, test_enrich = utils.feature_eng(train_enrich, val_enrich, test_enrich)
+
+
