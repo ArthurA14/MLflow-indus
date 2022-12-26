7a51caa8d3b7c7886502e3f205aea0bb2b219a34
Quentin ANGELOT <57401552+qangelot@users.noreply.github.com>
2022-12-26 20:43:01 +0100

    Update data_prep.py

--- files/data_prep.py
+++ files/data_prep.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-import mlflow
-import mlflow.sklearn
 import utils
 import warnings
 warnings.filterwarnings('ignore')
@@ -17,17 +15,8 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
-TRAIN_URL = r'../../data/application_train.csv'
-TEST_URL = r'../../data/application_test.csv' 
-
-
-#create experiement
-EXPERIMENT_NAME = "mlflow-demo"
-try:
-    EXPERIMENT_ID = mlflow.create_experiment(EXPERIMENT_NAME)
-except Exception as e:
-    logger.exception("Experiment ID already set. Error: %s", e)
-
+TRAIN_URL = r'../data/application_train.csv'
+TEST_URL = r'../data/application_test.csv' 
 
 try :
     # get data
@@ -37,43 +26,70 @@ except Exception as e :
     logger.exception("Unable to download training & test CSV. Error: %s", e)
 
 
-# Split the data into training and test sets. (0.80, 0.20) split.
-train, val = train_test_split(train, test_size=0.20, stratify=train['TARGET'], random_state=42)
+def data_prep(train, test):
+    """
+    Function to preprocess data
+    
+    Args: 
+        pd.DataFrame of train raw and test raw
+    
+    Returns:
+        pd.DataFrame of train, val and test pre processed
+    """
+
+    # Split the data into training and test sets. (0.80, 0.20) split.
+    train, val = train_test_split(train, test_size=0.20, stratify=train['TARGET'], random_state=42)
 
 
-############################ Encodage des variables cat #########################
+    ############################ Encodage des variables cat #########################
 
-train, val, test = utils.encoding(train, val, test)
+    train = utils.encoding(train, is_trainable=True)
+    val = utils.encoding(val, is_trainable=False)
+    test = utils.encoding(test, is_trainable=False) 
 
-############################ SOME DATAFRAME OPERATIONS ##########################
+    ############################ SOME DATAFRAME OPERATIONS ##########################
 
-# remove target 
-train_labels = train['TARGET']
-val_labels = val['TARGET']
+    # remove target 
+    train_labels = train['TARGET']
+    val_labels = val['TARGET']
 
-# keep only columns in both dfs
-train, test = train.align(test, join='inner', axis=1)
-train, val = train.align(val, join='inner', axis=1)
+    # keep only columns in both dfs
+    train, test = train.align(test, join='inner', axis=1)
+    train, val = train.align(val, join='inner', axis=1)
 
-# add back target
-train['TARGET'] = train_labels
-val['TARGET'] = val_labels
+    # add back target
+    train['TARGET'] = train_labels
+    val['TARGET'] = val_labels
 
-print('Train df shape: ', train.shape)
-print('Val df shape: ', val.shape)
-print('Test df shape: ', test.shape)
+    print('Train features shape after processing: ', train.shape)
+    print('Val features shape after processing: ', val.shape)
+    print('Test features shape after processing: ', test.shape)
 
 
-############################ SOME ANOMALIES OPERATIONS ##########################
+    ############################ SOME ANOMALIES OPERATIONS ##########################
+    
+    train = utils.fix_anomalies(train)
+    val = utils.fix_anomalies(val)
+    test = utils.fix_anomalies(test)
 
-# on indique les valeurs abbérantes detectées lors de l'EDA avec un booléen 
-train['DAYS_EMPLOYED_ANOMALIES'] = train["DAYS_EMPLOYED"] == 365243
 
-# on les remplace avec np.nan pour imputation ultérieure
-train['DAYS_EMPLOYED'].replace({365243: np.nan}, inplace = True)
+    ############################ ADDING BUSINESS FEATURES ###########################
 
-# on réplique les manipulations dans les dfs de val et de test
-val['DAYS_EMPLOYED_ANOMALIES'] = val["DAYS_EMPLOYED"] == 365243
-val["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
-test['DAYS_EMPLOYED_ANOMALIES'] = test["DAYS_EMPLOYED"] == 365243
-test["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
+    train_enrich = utils.feature_eng(train)
+    val_enrich = utils.feature_eng(val)
+    test_enrich = utils.feature_eng(test)
+    
+    URL = r'../data/train_enrich.csv'
+    utils.write_data(URL, train_enrich)
+    URL = r'../data/val_enrich.csv'
+    utils.write_data(URL, val_enrich)
+    URL = r'../data/test_enrich.csv'
+    utils.write_data(URL, test_enrich) 
+
+    return train, val, test
+
+
+if __name__ == "__main__" :
+    train, val, test = data_prep(train, test)
+
+        
