f559c2dbcb561faf57b54c03ce4aa41e4e6da174
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-12-27 01:51:40 +0100

    Update predict.py

--- files/predict.py
+++ files/predict.py
@@ -1,10 +1,9 @@
-from mlflow.tracking import MlflowClient
-from data_prep import logger
-import pandas as pd
-import numpy as np
 import joblib
+import pandas as pd
+from mlflow.tracking import MlflowClient
 import mlflow
 import utils
+import feature as ft
 
 
 EXPERIMENT_NAME = "mlflow-demo"
@@ -16,19 +15,20 @@ EXPERIMENT_ID = client.get_experiment_by_name(EXPERIMENT_NAME).experiment_id
 # Retrieve Runs information (parameter 'depth', metric 'accuracy')
 # ALL_RUNS_INFO = client.list_run_infos(EXPERIMENT_ID)
 # ALL_RUNS_ID = [run.run_id for run in ALL_RUNS_INFO]
+# ALL_PARAM = [client.get_run(run_id).data.params["class_weight"] for run_id in ALL_RUNS_ID]
 # ALL_PARAM = [client.get_run(run_id).data.params["C"] for run_id in ALL_RUNS_ID]
 # ALL_METRIC = [client.get_run(run_id).data.metrics["f1_score"] for run_id in ALL_RUNS_ID]
 # ALL_METRIC = [client.get_run(run_id).data.metrics["accuracy"] for run_id in ALL_RUNS_ID]
 
 
 # HERE: put the path of the best model (found on MLflow ui)
-logged_model = 'runs:/64d99d476e13491299604398404adae1/model'
+logged_model = 'runs:/f67e89851aed4bc686c997b78507e4bc/model'
 
-# Load model as a PyFuncModel.
+# Load model as a PyFuncModel
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 
 
-def predict(data: pd.DataFrame):
+def predict_(data: pd.DataFrame):
     """
     Helper to make predictions on data
     
@@ -40,37 +40,31 @@ def predict(data: pd.DataFrame):
     """
 
     # load necessary trained artefacts
-    imputer = joblib.load('imputer.save') 
-    scaler = joblib.load('std_scaler.save') 
+    imputer = joblib.load('../data/imputer.save') 
+    scaler = joblib.load('../data/std_scaler.save') 
     data = imputer.transform(data)
     data = scaler.transform(data)
 
-    return loaded_model.predict(data) 
+    return loaded_model.predict(data)
 
 
 if __name__ == "__main__" :
     with mlflow.start_run() :
 
-
-        TEST_URL = r'../data/test_enrich.csv' 
-        try :
-            # get data 
-            test_enrich = utils.get_data(TEST_URL)
-        except Exception as e :
-            logger.exception("Unable to download training & test CSV. Error: %s", e)
-
-        y_pred = predict(test_enrich)  
+        y_pred = predict_(ft.test_enrich) # y_pred = loaded_model.predict(X_test)
         y_pred = y_pred.reshape(y_pred.shape[0], 1)
 
         # Create the dataframe from numpy.ndarray
-        test_df = pd.DataFrame(test_enrich, columns=list(test_enrich.columns))
+        test_enrich_df = pd.DataFrame(ft.test_enrich, columns=list(ft.test_enrich.columns))
         y_pred_df = pd.DataFrame(y_pred, columns=['y_pred'])
 
-        # # Add y_pred column to the X_test dataset
-        test_df = test_df.join(y_pred_df) 
+        # Add y_pred column to the X_test dataset
+        test_df = test_enrich_df.join(y_pred_df)
 
-        URL = r'../data/final_test_df.csv'
-        utils.write_data(URL, test_df)
+
+        PATH = r'..\\data\\final_test_df.csv'
+        utils.write_data(PATH, test_df)
+
 
 
         # # Delete runs (DO NOT USE UNLESS CERTAIN)
