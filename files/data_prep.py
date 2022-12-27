33cff56f7fb39eb22fb091c1f710d37e3a0da0b7
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2022-12-27 01:50:44 +0100

    Update data_prep.py

--- files/data_prep.py
+++ files/data_prep.py
@@ -1,32 +1,35 @@
 import numpy as np
 import pandas as pd 
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import utils
 import warnings
-warnings.filterwarnings('ignore')
 import logging
+import utils
 
 
-# setting the seed
-np.random.seed(40)  
-
-# setting logger 
+# setting logger
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
+# setting warning
+warnings.filterwarnings("ignore")
 
-TRAIN_URL = r'../data/application_train.csv'
-TEST_URL = r'../data/application_test.csv' 
+# setting the seed 
+np.random.seed(40)  
+
+
+TRAIN_DF_PATH = r'..\\data\\application_train.csv'
+TEST_DF_PATH = r'..\\data\\application_test.csv' 
 
 try :
     # get data
-    train = utils.get_data(TRAIN_URL)
-    test = utils.get_data(TEST_URL)
+    train_df = utils.get_data(TRAIN_DF_PATH)
+    test_df = utils.get_data(TEST_DF_PATH)
 except Exception as e :
     logger.exception("Unable to download training & test CSV. Error: %s", e)
 
 
-def data_prep(train, test):
+def data_prep(train_df, test_df) :
     """
     Function to preprocess data
     
@@ -35,61 +38,53 @@ def data_prep(train, test):
     
     Returns:
         pd.DataFrame of train, val and test pre processed
-    """
+    """  
 
-    # Split the data into training and test sets. (0.80, 0.20) split.
-    train, val = train_test_split(train, test_size=0.20, stratify=train['TARGET'], random_state=42)
+    # Split the data into train_dfing and test_df sets. (0.80, 0.20) split.
+    train_df, val_df = train_test_split(train_df, test_size=0.20, stratify=train_df['TARGET'], random_state=42)
 
 
-    ############################ Encodage des variables cat #########################
+    ############################ DROP COLUMNS CONTAINING TOO MUCH NAN val_dfUES #########################
 
-    train = utils.encoding(train, is_trainable=True)
-    val = utils.encoding(val, is_trainable=False)
-    test = utils.encoding(test, is_trainable=False) 
+    train_df = utils.drop_column_with_nan(train_df)
+    val_df = utils.drop_column_with_nan(val_df)
+    test_df = utils.drop_column_with_nan(test_df)
+
+
+    ############################ ENCODING OF CATEGORICAL VARIABLES #########################
+
+    train_df = utils.encoding(train_df)
+    val_df = utils.encoding(val_df)
+    test_df = utils.encoding(test_df)
+
 
     ############################ SOME DATAFRAME OPERATIONS ##########################
 
     # remove target 
-    train_labels = train['TARGET']
-    val_labels = val['TARGET']
+    train_df_labels = train_df['TARGET']
+    val_df_labels = val_df['TARGET']
 
-    # keep only columns in both dfs
-    train, test = train.align(test, join='inner', axis=1)
-    train, val = train.align(val, join='inner', axis=1)
+    # keep only columns which are present in both dfs
+    train_df, test_df = train_df.align(test_df, join='inner', axis=1)
+    train_df, val_df = train_df.align(val_df, join='inner', axis=1)
 
     # add back target
-    train['TARGET'] = train_labels
-    val['TARGET'] = val_labels
+    train_df['TARGET'] = train_df_labels
+    val_df['TARGET'] = val_df_labels
 
-    print('Train features shape after processing: ', train.shape)
-    print('Val features shape after processing: ', val.shape)
-    print('Test features shape after processing: ', test.shape)
+    print('train_df shape after processing : ', train_df.shape)
+    print('val_df shape after processing : ', val_df.shape)
+    print('test_df shape after processing : ', test_df.shape)
 
 
     ############################ SOME ANOMALIES OPERATIONS ##########################
     
-    train = utils.fix_anomalies(train)
-    val = utils.fix_anomalies(val)
-    test = utils.fix_anomalies(test)
+    train_df = utils.fix_anomalies(train_df)
+    val_df = utils.fix_anomalies(val_df)
+    test_df = utils.fix_anomalies(test_df)
+
+    return train_df, val_df, test_df
 
 
-    ############################ ADDING BUSINESS FEATURES ###########################
-
-    train_enrich = utils.feature_eng(train)
-    val_enrich = utils.feature_eng(val)
-    test_enrich = utils.feature_eng(test)
-    
-    URL = r'../data/train_enrich.csv'
-    utils.write_data(URL, train_enrich)
-    URL = r'../data/val_enrich.csv'
-    utils.write_data(URL, val_enrich)
-    URL = r'../data/test_enrich.csv'
-    utils.write_data(URL, test_enrich) 
-
-    return train, val, test
-
-
-if __name__ == "__main__" :
-    train, val, test = data_prep(train, test)
-
-        
+# if __name__ == "__main__" :
+train_df, val_df, test_df = data_prep(train_df, test_df)
